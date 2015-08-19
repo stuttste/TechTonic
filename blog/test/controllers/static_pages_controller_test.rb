86eb4dfd79e6da3c6f0e1b490acc924dc53bfe76
Taylor Stutts <e_stutts@hotmail.com>
2015-08-19 09:40:58 -0500

    Finished chapter 3 in ROR

--- blog/test/controllers/static_pages_controller_test.rb
+++ blog/test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | TechTonic"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | TechTonic"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | TechTonic"
   end
 end
