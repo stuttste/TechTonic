b029858402076b2954ca85d624f711aacf888ae8
Taylor Stutts <e_stutts@hotmail.com>
2015-08-19 12:45:08 -0500

    Worked with page Titles

--- blog/test/controllers/static_pages_controller_test.rb
+++ blog/test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | TechTonic"
+    assert_select "title", "TechTonic"
   end
 
   test "should get help" do
